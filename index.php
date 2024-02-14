db976e5d743feea2d91d5f7839f1a280cc218bae
tusharWpc <nftusharwpcommerz@gmail.com>
2024-02-14 16:16:36 +0600

    data picker done

--- index.php
+++ index.php
@@ -9,8 +9,14 @@ Author: NF Tushar
 namespace Woocommerce_Preorders;
 
 class PreorderPlugin {
-    
+
+    // Constructor with existing plugin functionality
     public function __construct() {
+        // Existing plugin actions...
+        add_action( 'admin_menu', [$this, 'nf_preorder_plugin_menu'] );
+        add_action( 'admin_init', [$this, 'nf_preorder_plugin_settings'] );
+
+        // Additional actions for product customization and email handling
         add_action( 'woocommerce_product_after_variable_attributes', [$this, 'customVariationsFields'], 10, 3 );
         add_action( 'woocommerce_product_options_stock_status', [$this, 'customSimpleFields'] );
         add_action( 'woocommerce_save_product_variation', [$this, 'customVariationsFieldsSave'], 10, 2 );
@@ -23,11 +29,74 @@ class PreorderPlugin {
         add_action( 'woocommerce_order_status_completed', [$this, 'send_customer_email_for_preorder_product'], 10, 1 );
     }
 
-    // Enqueue jQuery UI Datepicker
-    public function enqueue_datepicker() {
-        wp_enqueue_script( 'jquery-ui-datepicker' );
-        wp_enqueue_style( 'jquery-ui-datepicker-style', 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css' );
+    // Add a menu item for your plugin in the dashboard
+    public function nf_preorder_plugin_menu() {
+        add_menu_page(
+            'NF Pre-Order Plugin Settings',
+            'Pre-Order Settings',
+            'manage_options',
+            'nf-preorder-plugin-settings',
+            [$this, 'nf_preorder_plugin_settings_page'],
+            'dashicons-cart', // Icon for your menu item, you can choose from WordPress dashicons
+            99 // Position of the menu item in the dashboard menu
+        );
     }
+
+    // Create the settings page content
+    public function nf_preorder_plugin_settings_page() {
+        ?>
+<div class="wrap">
+    <h2>NF Pre-Order Plugin Settings</h2>
+    <form method="post" action="options.php">
+        <?php
+                // Add nonce for security
+                settings_fields('nf_preorder_plugin_options');
+                // Output your settings fields
+                do_settings_sections('nf-preorder-plugin-settings');
+                // Add a submit button
+                submit_button('Save Settings');
+                ?>
+    </form>
+</div>
+<?php
+    }
+
+    // Register and initialize plugin settings
+    public function nf_preorder_plugin_settings() {
+        // Register a new setting for your plugin
+        register_setting('nf_preorder_plugin_options', 'nf_preorder_plugin_options');
+
+        // Add a section for your settings
+        add_settings_section(
+            'nf_preorder_plugin_general_section',
+            'General Settings',
+            [$this, 'nf_preorder_plugin_general_section_callback'],
+            'nf-preorder-plugin-settings'
+        );
+
+        // Add fields to the section
+        add_settings_field(
+            'preorder_email_template',
+            'Pre-Order Email Template',
+            [$this, 'preorder_email_template_callback'],
+            'nf-preorder-plugin-settings',
+            'nf_preorder_plugin_general_section'
+        );
+    }
+
+    // Callback function for the general settings section
+    public function nf_preorder_plugin_general_section_callback() {
+        echo '<p>Configure general settings for your NF Pre-Order Plugin.</p>';
+    }
+
+    // Callback function for the pre-order email template field
+    public function preorder_email_template_callback() {
+        $options = get_option('nf_preorder_plugin_options');
+        $template = isset($options['preorder_email_template']) ? $options['preorder_email_template'] : '';
+        echo '<textarea id="preorder_email_template" name="nf_preorder_plugin_options[preorder_email_template]" rows="5" cols="50">' . esc_textarea($template) . '</textarea>';
+    }
+
+ 
 
     // Schedule pre-order products
     public function schedule_preorder_products() {
@@ -164,7 +233,7 @@ class PreorderPlugin {
                 'id'          => '_preorder_available_date',
                 'label'       => __( 'Pre Order Available Date', 'pre-orders-for-woocommerce' ),
                 'placeholder' => date( 'Y-m-d h:i:s' ),
-                'class'       => 'datepicker',
+                'class'       => 'datepicker', // Add the class 'datepicker'
                 'desc_tip'    => true,
                 'description' => __( 'Choose when the product will be available for pre-order.', 'pre-orders-for-woocommerce' ),
                 'value'       => get_post_meta( get_the_ID(), '_preorder_available_date', true ),
@@ -175,7 +244,7 @@ class PreorderPlugin {
                 'id'          => '_preorder_end_date',
                 'label'       => __( 'Pre Order End Date', 'pre-orders-for-woocommerce' ),
                 'placeholder' => date( 'Y-m-d h:i:s' ),
-                'class'       => 'datepicker',
+                'class'       => 'datepicker', // Add the class 'datepicker'
                 'desc_tip'    => true,
                 'description' => __( 'Choose when the pre-order period ends.', 'pre-orders-for-woocommerce' ),
                 'value'       => get_post_meta( get_the_ID(), '_preorder_end_date', true ),
@@ -184,16 +253,22 @@ class PreorderPlugin {
         echo '</div>';
         echo '<div class="options_group form-row form-row-full hide_if_simple"></div>';
     }
-
+    
+    // Enqueue datepicker script
+    public function enqueue_datepicker() {
+        wp_enqueue_script( 'jquery-ui-datepicker' );
+        wp_enqueue_style( 'jquery-ui-datepicker-style', 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css' );
+        // Add additional script to initialize datepicker
+        wp_add_inline_script( 'jquery-ui-datepicker', 'jQuery(function($) { $(".datepicker").datepicker(); });' );
+    }
+    
     public function customVariationsFieldsSave( $post_id ) {
         $product = wc_get_product( $post_id );
         $is_preorder_variation = isset( $_POST['_is_preorder_' . $post_id] ) ? 'yes' : 'no';
         $product->update_meta_data( '_is_preorder', $is_preorder_variation );
         if ( $is_preorder_variation == 'yes' ) {
-            $pre_order_date_value = isset( $_POST['_pre_order_date_' . $post_id] ) ? esc_html( $_POST['_pre_order_date_' . $post_id] ) : '';
-            $product->update_meta_data( '_pre_order_date', esc_attr( $pre_order_date_value ) );
-        } else {
-            $product->update_meta_data( '_pre_order_date', '' );
+            $pre_order_date_value = isset( $_POST['_pre_order_date_' . $post_id] ) ? $_POST['_pre_order_date_' . $post_id] : '';
+            $product->update_meta_data( '_pre_order_date', $pre_order_date_value );
         }
         $product->save();
     }
@@ -203,19 +278,14 @@ class PreorderPlugin {
         $is_preorder = isset( $_POST['_is_preorder'] ) ? 'yes' : 'no';
         $product->update_meta_data( '_is_preorder', $is_preorder );
         if ( $is_preorder == 'yes' ) {
-            $preorder_available_date = isset( $_POST['_preorder_available_date'] ) ? esc_html( $_POST['_preorder_available_date'] ) : '';
-            $product->update_meta_data( '_preorder_available_date', esc_attr( $preorder_available_date ) );
+            $preorder_available_date = isset( $_POST['_preorder_available_date'] ) ? $_POST['_preorder_available_date'] : '';
+            $product->update_meta_data( '_preorder_available_date', $preorder_available_date );
 
-            $preorder_end_date = isset( $_POST['_preorder_end_date'] ) ? esc_html( $_POST['_preorder_end_date'] ) : '';
-            $product->update_meta_data( '_preorder_end_date', esc_attr( $preorder_end_date ) );
-        } else {
-            $product->update_meta_data( '_preorder_available_date', '' );
-            $product->update_meta_data( '_preorder_end_date', '' );
+            $preorder_end_date = isset( $_POST['_preorder_end_date'] ) ? $_POST['_preorder_end_date'] : '';
+            $product->update_meta_data( '_preorder_end_date', $preorder_end_date );
         }
         $product->save();
     }
-
 }
 
 new PreorderPlugin();
-?>
